022d4b650357a42338c11248ebcd1da6ba4bd990
matical <matic@amatsuka.me>
2020-10-04 01:01:38 +0800

    Update cli example, set cli group to None

--- cli.py
+++ cli.py
@@ -1,11 +1,12 @@
 import click
-from flask import Blueprint
+from flask import Blueprint, current_app
 
-cli = Blueprint("flaxen", __name__)
+cli = Blueprint("flaxen", __name__, cli_group=None)
 
 
 @cli.cli.command("stats")
-@click.argument("file")
+@click.option('--file', prompt='Which file?')
 def stats(file):
     """Help output here"""
     print(f"Stats for {file}")
+    print(current_app.instance_path)
