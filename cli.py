96493252e729fe96bad317a69b11af67d1920be2
matical <matic@amatsuka.me>
2020-10-05 02:11:48 +0800

    Command to empty cache

--- cli.py
+++ cli.py
@@ -1,7 +1,15 @@
 import click
 from flask import Blueprint, current_app
 
+from cache import cache
+
 cli = Blueprint("flaxen", __name__, cli_group=None)
+
+
+@cli.cli.command("clear-cache")
+def clear_cache():
+    cache.clear()
+    click.echo("Cache cleared!")
 
 
 @cli.cli.command("stats")
