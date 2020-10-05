98c6e54af1c620ed19672e81f308a6ad91c19946
matical <matic@amatsuka.me>
2020-10-06 01:25:45 +0800

    Geocode command

--- cli.py
+++ cli.py
@@ -2,14 +2,23 @@ import click
 from flask import Blueprint, current_app
 
 from cache import cache
+from koro.geo import resolve_coordinates
 
 cli = Blueprint("flaxen", __name__, cli_group=None)
 
 
 @cli.cli.command("clear-cache")
 def clear_cache():
+    """Clears the cache"""
     cache.clear()
     click.echo("Cache cleared!")
+
+
+@cli.cli.command("geocoord")
+@click.option("--location", prompt="Location")
+def geocoord(location):
+    """Resolve a coordinate based on a location name"""
+    print(resolve_coordinates(location))
 
 
 @cli.cli.command("stats")
