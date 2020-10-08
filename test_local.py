c0864a9cd284dde01b66d74b82f119a07129ae39
matical <matic@amatsuka.me>
2020-10-09 02:30:24 +0800

    More tests

---
+++ test_local.py
@@ -0,0 +1,21 @@
+from os import path
+
+import pytest
+from flask.testing import FlaskClient
+
+from factory import create_app
+from koro.manipulation import dataset_path
+
+
+@pytest.mark.usefixtures("config")
+def test_dataset_path(config):
+    base_path = config.root_path
+    assert dataset_path("static/test.csv") == path.join(
+        base_path, "raw_datasets/static/test.csv"
+    )
+    assert dataset_path("static", "test.csv") == path.join(
+        base_path, "raw_datasets/static/test.csv"
+    )
+    assert dataset_path("static", "deep/", "test.csv") == path.join(
+        base_path, "raw_datasets/static/deep/test.csv"
+    )
--- test_sanity.py
+++ test_sanity.py
@@ -1,17 +1,5 @@
-import pytest
 from flask import Response
 from flask.testing import FlaskClient
-
-from factory import create_app
-
-
-@pytest.fixture
-def client():
-    f = create_app()
-    with f.test_client() as client:
-        with f.app_context():
-            pass
-        yield client
 
 
 def test_home(client: FlaskClient):
