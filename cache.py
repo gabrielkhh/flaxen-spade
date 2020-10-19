9fef5511fea6a9d39fbfcce4e1a70cc513f95527
matical <matic@amatsuka.me>
2020-10-19 22:55:18 +0800

    Configurable redis url

--- cache.py
+++ cache.py
@@ -5,8 +5,8 @@ from flask_caching import Cache
 cache = Cache(
     config={
         "CACHE_TYPE": getenv("CACHE_TYPE", "simple"),
-        "CACHE_REDIS_URL": "redis://localhost:6379/0",
-        "CACHE_REDIS_PASSWORD": getenv("CACHE_REDIS_PASSWORD"),
+        "CACHE_REDIS_URL": getenv("REDIS_URL", "redis://localhost:6379/0"),
+        "CACHE_REDIS_PASSWORD": getenv("REDIS_PASSWORD"),
         "CACHE_KEY_PREFIX": "flaxen-spade",
     }
 )
