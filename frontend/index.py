f7c583632a76cbda4b6a5edd1deca843f24fcf43
matical <matic@amatsuka.me>
2020-10-20 17:27:47 +0800

    New routes

--- frontend/index.py
+++ frontend/index.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, current_app, render_template
+import jsonpickle
+from flask import Blueprint, Response, abort, current_app, jsonify, render_template
+from requests import HTTPError
 from werkzeug.local import LocalProxy
 
+from koro.datamall import Datamall
 from koro.resolve import BusServiceFactory
 
 logger = LocalProxy(lambda: current_app.logger)
@@ -13,13 +16,26 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/bus")
+def bus_index():
+    return render_template("bus/index.html")
+
+
 @app.route("/bus/service/<service>")
 def bus_service(service):
-    service = BusServiceFactory.load_service(service.upper())
+    try:
+        service = BusServiceFactory.load_service(service.upper())
+    except KeyError:
+        abort(404)
 
     return render_template("bus/service.html", service=service)
 
 
 @app.route("/bus/stop/<stop>")
 def bus_stop(stop):
-    pass
+    try:
+        stop = Datamall().bus_arrivals(stop)
+    except HTTPError as e:
+        abort(404)
+
+    return render_template("bus/stop.html", stop=stop.get_stop(), services=stop.all())
